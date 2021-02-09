6bf0e88edddc4286aad240d4160371152bddee54
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-02-08 21:11:32 -0500

    created aws service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AwsService} from "./aws.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AWS-UploadImages';
+  selectedFiles: FileList;
+  // currentFileUpload: File;
+  // progress: { percentage: number } = { percentage: 0 };
+  // selectedFile = null;
+  // changeImage = false;
+  // file: string;
+  //
+  // constructor(private uploadService: AwsService, private http: HttpClient) {}
+  //
+  // viewFile(){
+  //   window.open('https://bucketName.s3.cloudLocation.amazonaws.com/'+this.file);
+  // }
+  //
+  // deleteFile(){
+  //   this.http.post<string>
+  //   ('http://localhost:8080/deleteFile',this.file).subscribe(
+  //     res => {
+  //       this.file = res;
+  //     }
+  //   );
+  // }
+  //
+  // change(event){
+  //   this.changeImage = true;
+  // }
+  //
+  // changedImage(event) {
+  //   this.selectedFile = event.target.files[0];
+  // }
+  //
+  // upload(){
+  //   this.progress.percentage = 0;
+  //   this.currentFileUpload = this.selectedFile.item(0);
+  //
+  //   this.uploadService.pushFileToStorage(this.currentFileUpload).subscribe(event => {
+  //     this.selectedFiles = undefined;
+  //   });
+  // }
+  //
+  selectFile(event){
+    this.selectedFiles = event.target.files;
+  }
+
+
+
 }
