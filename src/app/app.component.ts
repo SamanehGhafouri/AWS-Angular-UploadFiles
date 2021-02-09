2710c524e0fb2b1bfec52cdcfa4a43c8ec05cc88
SamanehGhafouri <samaneh2010gh@gmail.com>
2021-02-09 12:59:58 -0500

    delete file working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,32 +12,32 @@ export class AppComponent {
   currentFileUpload: File;
   progress: { percentage: number } = { percentage: 0 };
   selectedFile = null;
-  // changeImage = false;
+  changeImage = false;
   file: string;
-  //
+
   constructor(private service: AwsService, private http: HttpClient) {}
 
   viewFile(){
     window.open('https://bucketName.s3.cloudLocation.amazonaws.com/'+this.file);
   }
-  //
-  // deleteFile(){
-  //   this.http.post<string>
-  //   ('http://localhost:8080/deleteFile',this.file).subscribe(
-  //     res => {
-  //       this.file = res;
-  //     }
-  //   );
-  // }
-  //
-  // change(event){
-  //   this.changeImage = true;
-  // }
-  //
-  // changedImage(event) {
-  //   this.selectedFile = event.target.files[0];
-  // }
-  //
+
+  deleteFile(){
+    this.http.post<string>
+    ('http://localhost:8080/deleteFile',this.file).subscribe(
+      res => {
+        this.file = res;
+      }
+    );
+  }
+
+  change(event){
+    this.changeImage = true;
+  }
+
+  changedImage(event) {
+    this.selectedFile = event.target.files[0];
+  }
+
   upload(){
     this.progress.percentage = 0;
     this.currentFileUpload = this.selectedFile.item(0);
